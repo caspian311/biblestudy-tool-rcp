87c60ded93a2e3f1645789b00ee6a9f58a596621
matt <matt@d5caad04-189b-467d-8c69-358c65116b6f>
2008-01-11 13:36:52 +0000

    Initial import.
    git-svn-id: http://svn.todd.net/svn/biblestudy/trunk@22 d5caad04-189b-467d-8c69-358c65116b6f

---
+++ net.todd.biblestudy.reference.nasb/src/net/todd/biblestudy/reference/nasb/NASBDataSource.java
@@ -0,0 +1,14 @@
+package net.todd.biblestudy.reference.nasb;
+
+import net.todd.biblestudy.reference.common.ReferenceDataSource;
+
+public class NASBDataSource extends ReferenceDataSource
+{
+	private static final String ID = "net.todd.biblestudy.reference.nasb";
+	
+	@Override
+	public String getId()
+	{
+		return ID;
+	}
+}
