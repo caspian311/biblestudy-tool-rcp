9798b86bc92fb1a26e3d9316b807bd79a1bdf6ed
matt <matt@d5caad04-189b-467d-8c69-358c65116b6f>
2008-01-15 13:56:52 +0000

    added test coverage to the models and started on the test coverage for the presenter in reference.common
    git-svn-id: http://svn.todd.net/svn/biblestudy/trunk@24 d5caad04-189b-467d-8c69-358c65116b6f

--- net.todd.biblestudy.reference.nasb/src/net/todd/biblestudy/reference/nasb/NASBDataSource.java
+++ net.todd.biblestudy.reference.nasb/src/net/todd/biblestudy/reference/nasb/NASBDataSource.java
@@ -1,14 +1,39 @@
 package net.todd.biblestudy.reference.nasb;
 
-import net.todd.biblestudy.reference.common.ReferenceDataSource;
+import java.util.List;
 
-public class NASBDataSource extends ReferenceDataSource
+import net.todd.biblestudy.reference.common.ReferenceDataSource;
+import net.todd.biblestudy.reference.common.ReferenceSearchResults;
+
+public class NASBDataSource implements ReferenceDataSource
 {
 	private static final String ID = "net.todd.biblestudy.reference.nasb";
 	
-	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see net.todd.biblestudy.reference.common.ReferenceDataSource#getId()
+	 */
 	public String getId()
 	{
 		return ID;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see net.todd.biblestudy.reference.common.ReferenceDataSource#getShortName()
+	 */
+	public String getShortName()
+	{
+		return "NASB";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see net.todd.biblestudy.reference.common.ReferenceDataSource#search(java.lang.String)
+	 */
+	public List<ReferenceSearchResults> search(String searchText)
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
