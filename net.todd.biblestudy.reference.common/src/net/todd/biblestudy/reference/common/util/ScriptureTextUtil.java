e69f1134a5dc3ea14ec65eab0be79ff36214444d
matt <matt@d5caad04-189b-467d-8c69-358c65116b6f>
2008-01-30 13:16:04 +0000

    made the Bible text wrap and re-wrap when the column is resized
    git-svn-id: http://svn.todd.net/svn/biblestudy/trunk@40 d5caad04-189b-467d-8c69-358c65116b6f

---
+++ net.todd.biblestudy.reference.common/src/net/todd/biblestudy/reference/common/util/ScriptureTextUtil.java
@@ -0,0 +1,54 @@
+package net.todd.biblestudy.reference.common.util;
+
+import java.util.StringTokenizer;
+
+
+public class ScriptureTextUtil
+{
+	public static String addNewLines(String text, int maxCharactersPerLine)
+	{
+		String newText = null;
+		
+		if (text != null)
+		{
+			text = text.replaceAll("\n", " ");
+			
+			StringBuffer sb = new StringBuffer();
+			
+			if (maxCharactersPerLine > 0)
+			{
+				StringTokenizer tokenizer = new StringTokenizer(text, " ");
+				
+				String firstWord = tokenizer.nextToken();
+				
+				sb.append(firstWord);
+				
+				int lengthOfLine = firstWord.length();
+				
+				while (tokenizer.hasMoreTokens())
+				{
+					String nextWord = tokenizer.nextToken();
+					
+					if (lengthOfLine + nextWord.length() + 1 > maxCharactersPerLine)
+					{
+						sb.append("\n").append(nextWord);
+						lengthOfLine = nextWord.length();
+					}
+					else
+					{
+						sb.append(" ").append(nextWord);
+						lengthOfLine += nextWord.length() + 1;
+					}
+				}
+				
+				newText = sb.toString();
+			}
+			else
+			{
+				newText = text;
+			}
+		}
+		
+		return newText;
+	}
+}
--- net.todd.biblestudy.reference.common/src/net/todd/biblestudy/reference/common/views/ReferenceView.java
+++ net.todd.biblestudy.reference.common/src/net/todd/biblestudy/reference/common/views/ReferenceView.java
@@ -9,7 +9,10 @@ import net.todd.biblestudy.common.ViewHelper;
 import net.todd.biblestudy.reference.common.BibleVerse;
 import net.todd.biblestudy.reference.common.ReferenceTransfer;
 import net.todd.biblestudy.reference.common.presenters.IReferenceViewListener;
+import net.todd.biblestudy.reference.common.util.ScriptureTextUtil;
 
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
@@ -18,26 +21,39 @@ import org.eclipse.swt.dnd.DragSource;
 import org.eclipse.swt.dnd.DragSourceAdapter;
 import org.eclipse.swt.dnd.DragSourceEvent;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.FontMetrics;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
 public class ReferenceView extends ViewPart implements IReferenceView
 {
+	protected static final String ID = "net.todd.biblestudy.reference.common.ReferenceView";
+	
 	private static final int TEXT_COLUMN_WIDTH = 200;
 	private static final String TEXT_COLUMN_HEADER = "Text";
 	private static final String REFERENCE_COLUMN_HEADER = "Reference";
 	private static final int REFERENCE_COLUMN_WIDTH = 100;
 
+	private int textColumnWidth = TEXT_COLUMN_WIDTH;
+	
 	private EventListenerList eventListeners = new EventListenerList();
 
 	private Combo referenceCombo;
@@ -49,11 +65,7 @@ public class ReferenceView extends ViewPart implements IReferenceView
 	private Label resultsMessage;
 	private Button lookupButton;
 	
-	protected static final String ID = "net.todd.biblestudy.reference.common.ReferenceView";
-
-	public ReferenceView() 
-	{
-	}
+	private static final int TEXT_MARGIN = 3;
 
 	@Override
 	public void createPartControl(Composite parent)
@@ -74,7 +86,7 @@ public class ReferenceView extends ViewPart implements IReferenceView
 	
 	private void createResultsArea(Composite parent)
 	{
-		resultsTableViewer = new TableViewer(parent, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.SHADOW_ETCHED_IN | SWT.FULL_SELECTION);
+		resultsTableViewer = new TableViewer(parent, SWT.BORDER | SWT.V_SCROLL | SWT.SHADOW_ETCHED_IN | SWT.FULL_SELECTION);
 		resultsTableViewer.setLabelProvider(new ResultsTableLabelProvider());
 		resultsTableViewer.setContentProvider(new ArrayContentProvider());
 		
@@ -83,19 +95,90 @@ public class ReferenceView extends ViewPart implements IReferenceView
 		resultsTable.setHeaderVisible(true);
 		resultsTable.setLinesVisible(true);
 		
+		resultsTable.addListener(SWT.MeasureItem, new Listener()
+		{
+			public void handleEvent(Event event)
+			{
+				TableItem item = (TableItem) event.item;
+				String text = item.getText(event.index);
+				Point size = event.gc.textExtent(text);
+				event.width = size.x + 2 * TEXT_MARGIN;
+				event.height = Math.max(event.height, size.y + TEXT_MARGIN);
+			}
+		});
+		resultsTable.addListener(SWT.EraseItem, new Listener()
+		{
+			public void handleEvent(Event event)
+			{
+				event.detail &= ~SWT.FOREGROUND;
+			}
+		});
+		resultsTable.addListener(SWT.PaintItem, new Listener()
+		{
+			public void handleEvent(Event event)
+			{
+				TableItem item = (TableItem) event.item;
+				String text = item.getText(event.index);
+				/* center column 1 vertically */
+				int yOffset = 0;
+				if (event.index == 1)
+				{
+					Point size = event.gc.textExtent(text);
+					yOffset = Math.max(0, (event.height - size.y) / 2);
+				}
+				event.gc.drawText(text, event.x + TEXT_MARGIN, event.y + yOffset, true);
+			}
+		});
+		
 		TableColumn referenceColumn = new TableColumn(resultsTable, SWT.LEFT);
 		referenceColumn.setText(REFERENCE_COLUMN_HEADER);
 		referenceColumn.setWidth(REFERENCE_COLUMN_WIDTH);
+		referenceColumn.setResizable(false);
 		
-		TableColumn textColumn = new TableColumn(resultsTable, SWT.VIRTUAL | SWT.LEFT);
+		TableColumn textColumn = new TableColumn(resultsTable, SWT.LEFT);
 		textColumn.setText(TEXT_COLUMN_HEADER);
 		textColumn.setWidth(TEXT_COLUMN_WIDTH);
+		textColumn.addControlListener(new ControlListener() 
+		{
+			public void controlMoved(ControlEvent e)
+			{
+			}
+
+			public void controlResized(ControlEvent e)
+			{
+				Rectangle clientArea = resultsTable.getClientArea();
+				
+				int tableWidth = clientArea.width - 100;
+				
+				textColumnWidth = tableWidth - REFERENCE_COLUMN_WIDTH;
+				
+				redoTheText();
+			}
+
+		});
 		
 		makeDragable();
-		
-		resultsTable.pack();
 	}
 
+	private void redoTheText()
+	{
+		GC gc = new GC(resultsTable);
+		FontMetrics fontMetrics = gc.getFontMetrics();
+		int averageCharWidth = fontMetrics.getAverageCharWidth();
+		gc.dispose();
+		
+		for (TableItem item : resultsTable.getItems())
+		{
+			String columnText = item.getText(1);
+			
+			int maxCharactersPerLine = textColumnWidth / averageCharWidth;
+			
+			String newColumnText = ScriptureTextUtil.addNewLines(columnText, maxCharactersPerLine);
+			
+			item.setText(1, newColumnText);
+		}
+	}
+	
 	private void makeDragable()
 	{
 		DragSource dragSource = new DragSource(resultsTableViewer.getTable(), DND.DROP_MOVE);
@@ -120,6 +203,8 @@ public class ReferenceView extends ViewPart implements IReferenceView
 			public void run()
 			{
 				resultsTableViewer.setInput(results);
+				
+				redoTheText();
 			}
 		});
 	}
@@ -245,4 +330,30 @@ public class ReferenceView extends ViewPart implements IReferenceView
 			}
 		});
 	}
+	
+//	class ResultsTableLabelProvider extends LabelProvider implements ITableLabelProvider
+//	{
+//		public Image getColumnImage(Object element, int columnIndex)
+//		{
+//			return null;
+//		}
+//
+//		public String getColumnText(Object element, int columnIndex)
+//		{
+//			BibleVerse result = (BibleVerse)element;
+//			
+//			String text = null;
+//			
+//			if (columnIndex == 0)
+//			{
+//				text = result.getReference().toString();
+//			}
+//			else if (columnIndex == 1)
+//			{
+//				text = result.getText();
+//			}
+//			
+//			return text;
+//		}
+//	}
 }
