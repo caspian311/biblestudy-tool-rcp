3108c133dcab2c211f3365b1584d3c53430b8ccd
matt <matt@d5caad04-189b-467d-8c69-358c65116b6f>
2008-02-29 13:25:43 +0000

    Initial import.
    git-svn-id: http://svn.todd.net/svn/biblestudy/trunk@83 d5caad04-189b-467d-8c69-358c65116b6f

---
+++ net.todd.biblestudy.rcp.test/src/net/todd/biblestudy/db/NoteDaoAdapter.java
@@ -0,0 +1,30 @@
+package net.todd.biblestudy.db;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class NoteDaoAdapter implements INoteDao
+{
+	public Note createNote(String newNoteName) throws SQLException
+	{
+		return null;
+	}
+
+	public void deleteNote(Note note) throws SQLException
+	{
+	}
+
+	public List<Note> getAllNotes() throws SQLException
+	{
+		return null;
+	}
+
+	public Note getNoteByName(String name) throws SQLException
+	{
+		return null;
+	}
+
+	public void saveNote(Note note) throws SQLException
+	{
+	}
+}
