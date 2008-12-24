bc82b511a022f7b0fd9f23fff71a069c821f202c
matt <matt@d5caad04-189b-467d-8c69-358c65116b6f>
2008-12-24 13:24:29 +0000

    Initial import.
    git-svn-id: http://svn.todd.net/svn/biblestudy/trunk@222 d5caad04-189b-467d-8c69-358c65116b6f

---
+++ notes-client/src/main/java/net/todd/biblestudy/client/BibleStudyClient.java
@@ -0,0 +1,35 @@
+package net.todd.biblestudy.client;
+
+import java.net.MalformedURLException;
+
+import net.todd.biblestudy.BibleStudyService;
+import net.todd.biblestudy.Verse;
+import net.todd.biblestudy.cli.Note;
+
+import org.codehaus.xfire.client.XFireProxyFactory;
+import org.codehaus.xfire.service.Service;
+import org.codehaus.xfire.service.binding.ObjectServiceFactory;
+
+public class BibleStudyClient implements BibleStudyService {
+	private BibleStudyService service = null;
+	public BibleStudyClient() {
+		Service serviceModel = new ObjectServiceFactory()
+				.create(BibleStudyService.class);
+
+		try {
+			service = (BibleStudyService) new XFireProxyFactory()
+					.create(serviceModel,
+							"http://localhost:8080/notes-service/services/BibleStudyServiceImpl");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Verse[] searchForReference(String query) {
+		return service.searchForReference(query);
+	}
+
+	public Note[] searchForNote(String query) {
+		return service.searchForNote(query);
+	}
+}
