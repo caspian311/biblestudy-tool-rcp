2546d79b13e3ca02306c8d5f0a649818dbd44006
matt.todd <matt.c.todd@gmail.com>
2009-08-17 08:10:12 -0500

    removing non-rcp stuffs

--- bible-db/src/main/java/net/todd/biblestudy/IContentProvider.java
+++
@@ -1,7 +0,0 @@
-package net.todd.biblestudy;
-
-import java.util.List;
-
-public interface IContentProvider {
-	List<SearchableData> getData();
-}
--- bible-db/src/main/java/net/todd/biblestudy/ISearchEngine.java
+++
@@ -1,7 +0,0 @@
-package net.todd.biblestudy;
-
-public interface ISearchEngine {
-	void index(String indexLocation);
-
-	SearchResult[] search(String queryString) throws SearchException;
-}
